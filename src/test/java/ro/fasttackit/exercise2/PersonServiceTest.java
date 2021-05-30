28fcfc71a76539b012911515784d06dceb4163fa
Horatiu Pacurar <horatiupacurar20@gmail.com>
2021-05-30 17:04:36 +0300

    second commit

--- src/test/java/ro/fasttackit/exercise2/PersonServiceTest.java
+++ src/test/java/ro/fasttackit/exercise2/PersonServiceTest.java
@@ -1,4 +1,19 @@
 package ro.fasttackit.exercise2;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ro.fasttrackit.exercise2.PersonService;
+
 public class PersonServiceTest {
+	PersonService personService;
+
+	@BeforeEach
+	void setup(){
+		personService = new PersonService(); //ce parametrii ar trebui sa inserez aici ?
+	}
+
+	@Test
+	@DisplayName("")
+
 }
